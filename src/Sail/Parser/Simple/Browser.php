4947d803fc4f1ff51732452f6d6ae50394aace84
Hadar Porat <hadar.porat@realpage.com>
2015-10-29 13:27:00 -0400

    added microsoft edge detection

--- src/Sail/Parser/Simple/Browser.php
+++ src/Sail/Parser/Simple/Browser.php
@@ -4,10 +4,11 @@ namespace Sail\Parser\Simple;
 
 class Browser
 {
-    
+
     protected static $browser_list = array(
         'Chrome'  => 'Chrome',
         'Firefox' => 'Firefox',
+        'Edge'    => 'Edge',
         'MSIE'    => 'MSIE',
         'Safari'  => 'Safari',
         'Unknown' => 'Unknown'
@@ -15,9 +16,6 @@ class Browser
 
     public static function parse($ua)
     {
-        $name = null;
-        $version = null;
-        
         // get the browser
         foreach (self::$browser_list as $preg_id => $id) {
             if ( strpos($ua, $preg_id) ) {
@@ -39,29 +37,34 @@ class Browser
                 break;
             default:
                 preg_match("#$id/([^\s]*)#", $ua, $match);
-                if(count($match)>0){
-                    $version = $match[1];
-                    $name = "$id $version";
-                }
+                $version = $match[1];
+                $name = "$id $version";
         }
-        
+
         // handle IE11 new user agent
         preg_match('/MSIE (.*?);/', $ua, $matches);
         if (count($matches)<2){
             preg_match('/Trident\/\d{1,2}.\d{1,2}; rv:([0-9]*)/', $ua, $matches);
         }
-        
+
         if (count($matches)>1){
             // then we're using IE
             $id = 'MSIE';
             $name = 'MSIE';
             $version = $matches[1];
         }
-        
+
         if (strpos($ua, 'Trident/7.0') !== false && strpos($ua, 'rv:11.0') !== false) {
             $id = 'MSIE';
-            $name = 'MSIE';
+            $name = 'MSIE 11';
             $version = '11';
+        }
+
+        if (strpos($ua, 'Edge') !== false) {
+            $id = 'Edge';
+            preg_match("#$id/([^\s]*)#", $ua, $match);
+            $version = $match[1];
+            $name = "$id $version";
         }
 
         return array(
@@ -69,6 +72,6 @@ class Browser
             'name'    => $name,
             'version' => $version,
         );
-   
+
     }
 }
